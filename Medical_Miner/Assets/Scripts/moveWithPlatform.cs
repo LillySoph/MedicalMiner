931385a3d2e7a0782cc86185b016b0929c3717dd
Mark <stinnermaximilian@gmail.com>
2021-04-03 15:33:43 +0200

    movingPlatforms

---
+++ Medical_Miner/Assets/Scripts/moveWithPlatform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveWithPlatform : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag == "movingPlatform")
+        {
+            transform.parent = other.transform;
+            Debug.Log("We are on a moving platform");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.transform.tag == "movingPlatform")
+        {
+            transform.parent = null;
+        }
+    }
+}
