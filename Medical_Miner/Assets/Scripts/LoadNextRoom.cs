3b857c10dfd42391ef4faa0500981bffa2c3ec50
Lilly Schumann <lillyschumann@googlemail.com>
2021-04-06 16:35:29 +0200

    Nächsten Raum innerhalb Level zufällig aus Raum-Pool auswählen

--- Medical_Miner/Assets/Scripts/LoadNextRoom.cs
+++ Medical_Miner/Assets/Scripts/LoadNextRoom.cs
@@ -5,10 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LoadNextRoom : MonoBehaviour
 {
+    // [SerializeField]
+    private int roomAmountPerLevel = 10;
+
+    [SerializeField]
+    private int firstRoomOfLevelIndex = 1;
+
+    [SerializeField]
+    private int endscreenOfLevelIndex = 6;
+    
+    private GameMaster gameMaster;
+    private int roomIndexOfBefore;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        gameMaster = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
     }
 
     // Update is called once per frame
@@ -19,7 +31,33 @@ public class LoadNextRoom : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int roomsVisited = gameMaster.roomsVisited;
+        // load next room in same level
+        if (roomsVisited < roomAmountPerLevel) {
+            int index = getIndexOfNextRoom();
+            SceneManager.LoadScene(index);
+            Debug.Log("Room Index: " + index + " | " + roomsVisited + "/" + roomAmountPerLevel);
+            gameMaster.roomsVisited++;
+        } 
+        // load endscreen of level
+        else {
+            SceneManager.LoadScene(endscreenOfLevelIndex); 
+            // reset room visited counter
+            gameMaster.roomsVisited = 0;
+        }
+        
     }
 
+    int getIndexOfNextRoom() 
+    {
+        int current = SceneManager.GetActiveScene().buildIndex;
+        int next;
+        // int before = roomIndexOfBefore
+        do {
+            next = Random.Range(firstRoomOfLevelIndex + 2, firstRoomOfLevelIndex + 5);
+        } while (next == current);
+
+        return next;
+    }
 }
